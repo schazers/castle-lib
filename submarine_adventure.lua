537c27c050fbf03132ae709d2462da5955ac6079
schazers <schazers@gmail.com>
2019-05-15 03:23:38 -0700

    cram submarine_adventure within 2 tweets

--- submarine_adventure.lua
+++ submarine_adventure.lua
@@ -1,36 +1,24 @@
 require 'bits'
-N,X,Y,c,spd,A,sc,F=17,2,9,5,.06,11,0,{}
-function _L()
-  for i=1,N do F[i]={h=A,c=9} end
+TS,N,X,Y,c,p,e,F=1,17,2,9,5,.06,0,{}
+function _L() 
+for i=1,N do F[i]={t=3,h=10} end 
 end
-function _D()
-  if GO then BG(3) else BG(6) B(3,Y,c) end
-  for i=1,N do
-    q=F[i] 
-    wall_color = GO and 11 or 7
-    V(i,0,q.c-q.h/2,wall_color) -- top of this wall
-    V(i,q.c+CEIL(q.h/2),N,wall_color) -- bottom of this wall
-  end
-  TEXT(GO and sc or "",20,420,6,1)
+function _D() 
+BG(GO and 11 or 7)
+for i=1,N do q=F[i] V(i,q.t,q.t+q.h,GO and 3 or 6) end
+B(4,Y,c)
+TEXT(GO and e or "",20,420,6,1)
 end
 function _U(dt)
-  -- DIE
-  --CB()
-  dw=F[3]
-  if CEIL(Y)<dw.c-dw.h/2+1 or FLR(Y)>dw.c+dw.h/2 then DIE() end
-
-  if not GO then
-    -- BPM flash
-    if T(1)>0.395 then c=8 else c=5 end
-    if T(1)>0.51715 then sc=sc+1 RT(1) end
-
-    -- move F
-    spd=spd-.0015*dt
-    A=A-0.14*dt
-    if T(2)>MAX(spd,.033) then
-      for i=1,N-1 do F[i].c=F[i+1].c F[i].h=F[i+1].h end
-      F[N]={c=CL(3, F[N-1].c+RN(-1,1), N-3),h=CEIL(5+100*spd)}
-      RT(2)
-    end
-  end
+q=F[4]
+if not GO and Y>q.t and Y<q.t+q.h+1 then
+if T(1)>.395 then c=8 end
+if T(1)>.51715 then c=5 e=e+1 RT(1) end
+p=p-.0015*dt
+if T(2)>MAX(p,.033) then
+for i=1,N-1 do F[i]=F[i+1] end
+F[N]={t=CL(-2,F[N-1].t+RN(-1,1),N-3),h=CEIL(5+100*p)}
+RT(2)
+end
+else DIE() end
 end
